d7ffb60715a81dcacb7d40d50ce20acc57e9d312
ahhbee0127 <ahhbee_0127@hotmail.com>
2018-07-21 20:33:01 +0800

    0006 - Added navigation slide bar in main activity.

--- NutritionClub/app/src/main/java/com/example/nutritionclub/AccountActivity/LoginActivity.java
+++ NutritionClub/app/src/main/java/com/example/nutritionclub/AccountActivity/LoginActivity.java
@@ -32,8 +32,8 @@ public class LoginActivity extends AppCompatActivity {
         auth = FirebaseAuth.getInstance();
 
         if (auth.getCurrentUser() != null) {
-            //startActivity(new Intent(LoginActivity.this, MainActivity.class));
-            startActivity(new Intent(LoginActivity.this, CalculateFatActivity.class));
+            startActivity(new Intent(LoginActivity.this, MainActivity.class));
+            //startActivity(new Intent(LoginActivity.this, CalculateFatActivity.class));
             finish();
         }
 
@@ -97,9 +97,9 @@ public class LoginActivity extends AppCompatActivity {
                                         Toast.makeText(LoginActivity.this, getString(R.string.auth_failed), Toast.LENGTH_LONG).show();
                                     }
                                 } else {
-//                                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-//                                    startActivity(intent);
-                                    startActivity(new Intent(LoginActivity.this, CalculateFatActivity.class));
+                                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                    startActivity(intent);
+                                    //startActivity(new Intent(LoginActivity.this, CalculateFatActivity.class));
                                     finish();
                                 }
                             }
